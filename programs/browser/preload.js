9f60a54546cce3cb8a06990a7875c442c1497e59
Jake <jakekohl523@gmail.com>
2025-07-24 05:29:32 -0400

    Enable extension installation via zip and manage from UI

--- programs/browser/preload.js
+++ programs/browser/preload.js
@@ -7,7 +7,7 @@ contextBridge.exposeInMainWorld('electron', {
     // Function to send data from renderer to main
     send: (channel, data) => {
         // Whitelist channels to prevent sending on arbitrary channels
-        const validChannels = ['navigate-to-url', 'set-browser-zoom', 'browser-go-back', 'browser-go-forward'];
+        const validChannels = ['navigate-to-url', 'set-browser-zoom', 'browser-go-back', 'browser-go-forward', 'open-extension-options'];
         if (validChannels.includes(channel)) {
             ipcRenderer.send(channel, data);
         }
@@ -21,7 +21,7 @@ contextBridge.exposeInMainWorld('electron', {
         }
     },
     invoke: (channel, data) => {
-        const validChannels = ['install-extension'];
+        const validChannels = ['install-extension', 'get-installed-extensions'];
         if (validChannels.includes(channel)) {
             return ipcRenderer.invoke(channel, data);
         }
